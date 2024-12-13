69ddfe33449e46d271b051141a9ac4b6f07e8673
Flynn Duniho <flynn@harveyosity.org>
2024-12-13 00:01:24 -0500

    Add a QR code pdf

--- Once Upon A/Assets/Scripts/CameraController.cs
+++ Once Upon A/Assets/Scripts/CameraController.cs
@@ -7,72 +7,72 @@ using static Utils.Utils;
 
 public class CameraController : MonoBehaviour
 {
-  public GameObject Target;
-  public float HoriThreshold = 2;
-  public float VertThreshold = 5;
-  public float Speed = 20;
+    public GameObject Target;
+    public float HoriThreshold = 2;
+    public float VertThreshold = 5;
+    public float Speed = 20;
 
-  public float ZoomSpeed = 0.5f;
+    public float ZoomSpeed = 0.5f;
 
-  private Camera cam;
-  private float normalSize;
-  private ExpDamp zoom;
-  private ExpDampVec3 position;
+    private Camera cam;
+    private float normalSize;
+    private ExpDamp zoom;
+    private ExpDampVec3 position;
 
-  void Awake()
-  {
-    cam = GetComponent<Camera>();
-    normalSize = cam.orthographicSize;
-    zoom = new(normalSize, normalSize, () => { cam.orthographicSize = zoom?.Value ?? 1; transform.localScale = Vector3.one * cam.orthographicSize; });
-    position = new(Vector3.zero, Vector3.zero, () => transform.position = position?.Value ?? Vector3.zero);
-  }
-
-  void Start()
-  {
-    position.TargetValue = Target.transform.position;
-    position.Value = Target.transform.position;
-  }
-
-  void Update()
-  {
-    if (GameManager.Manager.IsPaused)
+    void Awake()
     {
-      zoom.TargetValue = normalSize * 1.1f;
-    }
-    else
-    {
-      zoom.TargetValue = normalSize;
+        cam = GetComponent<Camera>();
+        normalSize = cam.orthographicSize;
+        zoom = new(normalSize, normalSize, () => { cam.orthographicSize = zoom?.Value ?? 1; transform.localScale = Vector3.one * cam.orthographicSize; });
+        position = new(Vector3.zero, Vector3.zero, () => transform.position = position?.Value ?? Vector3.zero);
     }
 
-    zoom.Next(ZoomSpeed, Time.unscaledDeltaTime);
-
-    /* if (GameManager.Manager.IsPaused) { return; } */
-  }
-
-  void FixedUpdate()
-  {
-    Vector2 deltaPos = Target.transform.position - transform.position;
-    float x = transform.position.x, y = transform.position.y;
-
-    if (deltaPos.x > HoriThreshold)
+    void Start()
     {
-      x = Target.transform.position.x - HoriThreshold;
-    }
-    else if (deltaPos.x < -HoriThreshold)
-    {
-      x = Target.transform.position.x + HoriThreshold;
+        position.TargetValue = Target.transform.position;
+        position.Value = Target.transform.position;
     }
 
-    if (deltaPos.y > VertThreshold)
+    void Update()
     {
-      y = Target.transform.position.y - VertThreshold;
-    }
-    else if (deltaPos.y < -VertThreshold)
-    {
-      y = Target.transform.position.y + VertThreshold;
+        if (GameManager.Manager.IsPaused)
+        {
+            zoom.TargetValue = normalSize * 1.1f;
+        }
+        else
+        {
+            zoom.TargetValue = normalSize;
+        }
+
+        zoom.Next(ZoomSpeed, Time.unscaledDeltaTime);
+
+        /* if (GameManager.Manager.IsPaused) { return; } */
     }
 
-    position.TargetValue = new Vector3(x, y, -100);
-    position.Next(Speed, Time.fixedDeltaTime);
-  }
+    void FixedUpdate()
+    {
+        Vector2 deltaPos = Target.transform.position - transform.position;
+        float x = transform.position.x, y = transform.position.y;
+
+        if (deltaPos.x > HoriThreshold)
+        {
+            x = Target.transform.position.x - HoriThreshold;
+        }
+        else if (deltaPos.x < -HoriThreshold)
+        {
+            x = Target.transform.position.x + HoriThreshold;
+        }
+
+        if (deltaPos.y > VertThreshold)
+        {
+            y = Target.transform.position.y - VertThreshold;
+        }
+        else if (deltaPos.y < -VertThreshold)
+        {
+            y = Target.transform.position.y + VertThreshold;
+        }
+
+        position.TargetValue = new Vector3(x, y, -100);
+        position.Next(Speed, Time.fixedDeltaTime);
+    }
 }
